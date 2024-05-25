9f8a2cddc973f6b6c03534014b1fa1035afe44e7
domingo2000 <dedwards@buk.cl>
2024-05-25 16:00:22 -0400

    feat: added formatter

--- src/lib/names.ts
+++ src/lib/names.ts
@@ -1,6 +1,4 @@
 export async function getName() {
-    const { faker } = await import(
-        "@faker-js/faker/locale/en_US"
-    );
-    return faker.person.fullName();
+  const { faker } = await import("@faker-js/faker/locale/en_US");
+  return faker.person.fullName();
 }
