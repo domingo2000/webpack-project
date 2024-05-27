472837757d4b66db45996d80d43ba553359ed1cd
domingo2000 <dedwards@buk.cl>
2024-05-27 19:44:51 -0400

    feat: added full pwa support for github pages deployment with vite and vite pwa plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 /** @type {import('vite').UserConfig} */
 export default {
   root: "src",
+  base: "/webpack-project/",
   publicDir: "../public",
   build: {
     outDir: "../dist",
@@ -16,9 +17,38 @@ export default {
     compression(),
     react(),
     VitePWA({
-      injectRegister: "auto",
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg}"],
+      },
+      manifest: {
+        name: "Vite App",
+        short_name: "Vite",
+        description: "A Vite app with PWA",
+        start_url: "/webpack-project/",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
       },
     }),
   ],
